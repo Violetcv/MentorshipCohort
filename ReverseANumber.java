1818fd0a33ac394bb84fd64d57dcbb86b674a6b4
Violetcv <97514667+Violetcv@users.noreply.github.com>
2023-01-19 12:01:09 +0530

    Submitting Assignment 1 Question 6 ReverseANumber

---
+++ ReverseANumber.java
@@ -0,0 +1,19 @@
+import java.util.*;
+   
+   public class ReverseANumber{
+   
+   public static void main(String[] args) {
+    Scanner scn = new Scanner(System.in);
+    // write ur code here
+    System.out.print("");
+    int n = scn.nextInt();
+    int reversed = 0;
+    while(n!= 0){
+        int digit = n%10;
+        reversed = reversed * 10 + digit;
+        n/=10;
+        }
+    System.out.println(reversed);
+       
+   }
+   }
