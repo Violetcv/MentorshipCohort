1424b0143657d9bf90461d0679682a7f10c495c8
Violetcv <97514667+Violetcv@users.noreply.github.com>
2023-01-19 12:30:40 +0530

    Update PrintZ.java

--- PrintZ.java
+++ PrintZ.java
@@ -1,6 +1,6 @@
 
 class PrintZ{
-  public static void main(String[] args) {
+  public static void PrintZ(String[] args) {
     for (int i = 0; i < 5; i++) {
         for (int j = 0; j < 5; j++) {
             if (i == 0 || i == 4 || j == 4 - i){
